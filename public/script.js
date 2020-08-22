276f91fdd492c7f99903cadb67dfa683d2247dae
Ariska Hidayat <ariska138@gmail.com>
2020-08-22 12:18:53 +0700

    response menu

--- public/script.js
+++ public/script.js
@@ -2,12 +2,16 @@ var isShowMenu = false;
 
 function navMenuMobile() {
     let menu = document.getElementById('menu');
+    let iconMenu = document.getElementById('icon-menu');
     if (isShowMenu) {
-        menu.classList.add('hidden');
+        menu.style.display = "none";
+        iconMenu.classList.remove('fa-close');
+        iconMenu.classList.add('fa-bars');
     } else {
-        menu.classList.remove('hidden');
+        menu.style.display = "grid";
+        iconMenu.classList.remove('fa-bars');
+        iconMenu.classList.add('fa-close');
     }
 
     isShowMenu = !isShowMenu;
-   console.log('menu');
 }
