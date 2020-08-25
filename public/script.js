2dadad1a1c62fead0d8056a4ab704061c0dd56c6
Ariska Hidayat <ariska138@gmail.com>
2020-08-26 06:51:04 +0700

    fix layout

--- public/script.js
+++ public/script.js
@@ -25,12 +25,22 @@ window.onresize = reportWindowSize;
 window.onscroll = function() { myFunction() };
 
 var navbar = document.getElementById("navbar");
+var sidebar = document.getElementById("sidebar");
+
 var posStickyActive = navbar.offsetTop;
+var posActiveSidebar = sidebar.offsetTop;
 
 function myFunction() {
+    // console.log(window.pageYOffset + ' ' + posStickyActive);
     if (window.pageYOffset >= posStickyActive) {
-        navbar.classList.add("sticky")
+        navbar.classList.add("position-fixed--fullwidth")
     } else {
-        navbar.classList.remove("sticky");
+        navbar.classList.remove("position-fixed--fullwidth");
+    }
+
+    if (window.pageYOffset >= posStickyActive && window.innerWidth > 768) {
+        sidebar.classList.add("position-fixed--sidebar")
+    } else {
+        sidebar.classList.remove("position-fixed--sidebar");
     }
 }
